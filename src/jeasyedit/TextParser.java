b02ccba31f106953a658d21de8cdbba82ca756d6
Patrik Bjornfot <patrik.bjornfot@gmail.com>
2009-04-04 20:53:05 +0200

    first commit

---
+++ src/jeasyedit/TextParser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jeasyedit;
+
+/**
+ *This is the parser class for the text object...
+ * @author patrik
+ */
+public class TextParser {
+
+}
